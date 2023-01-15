269d7b57bce79179a32af64cea7b81811010b3d7
dhiazulfa <dhiazulfamb@gmail.com>
2023-01-15 11:14:00 +0700

    merge main

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,12 +18,20 @@ class HomeController extends Controller
 
         if(request('category')){
             $category = Category::firstWhere('slug', request('category'));
+            if(!$category){
+                abort(404);
+            } else{
             $title = ' in ' . $category->name;
+            }
         }
 
         if(request('user')){
             $user = User::firstWhere('username', request('user'));
-            $title = ' by ' . $user->name;
+            if(!$user){
+                abort(404);
+            } else{
+            $title = ' in ' . $user->name;
+            }
         }
 
         return view('index', [
@@ -31,9 +39,9 @@ class HomeController extends Controller
             "active" => "home",
             "posts" => Post::latest()->filter(request(['search', 'category', 'user']))->paginate(4)->withQueryString(),
             "posts2" => Post::latest()->filter(request(['search', 'category', 'user']))->paginate(8)->withQueryString(),
-
+            
             "videos" => Video::latest()->filter(request(['search', 'category', 'user']))->paginate(4)->withQueryString(),
-            "videos_latest" => Video::latest()->filter(request(['search', 'category', 'user']))->paginate(5)->withQueryString(),
+            "videos_latest" => Video::latest()->filter(request(['search', 'category', 'user']))->paginate(8)->withQueryString(),
             "categories" => Category::all(),
         ]);
     }
--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -16,18 +16,26 @@ class VideoController extends Controller
 
         if(request('category')){
             $category = Category::firstWhere('slug', request('category'));
+            if(!$category){
+                abort(404,'Category Not Found!');
+            } else{
             $title = ' in ' . $category->name;
+            }
         }
 
         if(request('user')){
             $user = User::firstWhere('username', request('user'));
+            if(!$category){
+                abort(404,'Category Not Found!');
+            } else{
             $title = ' by ' . $user->name;
+            }
         }
 
         return view('videos', [
             "title" => "All Videos" . $title,
             "active" => 'videos',
-            "videos" => Video::latest()->filter(request(['search', 'category', 'user']))->paginate(3)->withQueryString()
+            "videos" => Videos::latest()->filter(request(['search', 'category', 'user']))->paginate(3)->withQueryString()
         ]);
     }
 
